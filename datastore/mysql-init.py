c3013e4e42e27f6c79621b9e259a67322c02c59c
服部大地 <p3q24c2o@s.okayama-u.ac.jp>
2020-05-13 11:30:56 +0900

    merge/pull request

--- datastore/mysql-init.py
+++ datastore/mysql-init.py
@@ -12,14 +12,20 @@ db=MySQL(**dns)
 
 db._open()
 cursor=db.dbh.cursor()
+cursor.execute('DROP TABLE IF EXISTS measurement')
 cursor.execute('CREATE TABLE IF NOT EXISTS measurement(deviceId CHAR(6),time INT,temperature FLOAT,humid FLOAT,pressure INT,distance INT,warninglevel INT)')
+
+cursor.execute('DROP TABLE IF EXISTS devicelist')
 cursor.execute('CREATE TABLE IF NOT EXISTS devicelist(deviceId CHAR(6) NOT NULL,address VARCHAR(30),longitude double(9,6), latitude FLOAT(8,6), max_tempreture FLOAT, max_humid FLOAT, max_pressure INT, max_distance INT,PRIMARY KEY(deviceId))')
 
+cursor.execute('DROP TABLE IF EXISTS predict')
 cursor.execute('CREATE TABLE IF NOT EXISTS predict(deviceId CHAR(6),time INT,temperature FLOAT,humid FLOAT,pressure INT,distance INT,warninglevel INT)')
 
 #値を代入
-db.insert_devicelist('759E68','point1','133.993549','34.662863','35.0','80.0','1020','30')
-db.insert_devicelist('742879','point2','133.921242','34.689664','35.0','80.0','1020','30')
+# db.insert_devicelist('759E68','point1','133.993549','34.662863','35.0','80.0','1020','30')
+# db.insert_devicelist('742879','point2','133.921242','34.689664','35.0','80.0','1020','30')
+cursor.execute("INSERT INTO devicelist VALUES('759E68','point1', 133.993549, 34.662863, 35.0, 80.0, 1020, 30)")
+cursor.execute("INSERT INTO devicelist VALUES('742879','point2', 133.921242, 34.689664, 35.0, 80.0, 1020, 30)")
 
 cursor.close()
 db.dbh.commit()
